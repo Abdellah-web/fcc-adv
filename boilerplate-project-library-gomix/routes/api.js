ce2bcc0f67ba0f84d2f15e3b9a15eae7045a8b65
Abdellah-web <54070353+Abdellah-web@users.noreply.github.com>
2019-09-16 15:13:21 +0200

    Update api.js

--- boilerplate-project-library-gomix/routes/api.js
+++ boilerplate-project-library-gomix/routes/api.js
@@ -56,10 +56,29 @@ module.exports = function (app) {
     .post(function (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
-    })
+  if (!title) {
+        return res.send('invalid title')
+      } else {
+        const bookDetails = {
+          "title": title, updated_on: Date.now( )
+        }
+        const newBookAdded = new booksModel(bookDetails);
+
+        newBookAdded.save();
+
+        res.json(newBookAdded);
+      }
+  
+  })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+     bookModel.remove({},function(err){
+     if(err){
+          return res.send('complete delete failed');
+        }
+        return res.send('complete delete successful');
+     })
     });
 
 
@@ -68,17 +87,62 @@ module.exports = function (app) {
     .get(function (req, res){
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    if (!bookid) {
+        return res.send('please put id')
+      }
+    bookModel.findById({ _id: bookid }, function(err,book){
+    if (err){
+    res.send('err when searching book byId')
+    }  
+     if(!book) {
+          res.send('no book exists');
+    }
+        return res.json({_id: book._id, title: book.title, comments: book.comments });
     })
+  })
     
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+    if (!bookid) {
+        return res.send('please put id')
+      }
+    if(!comment || comment === ''){
+      return res.send('please put comment')
+    }
+    bookModel.findById({ _id: bookid }, function(err, book){
+    if (err){
+    res.send('err when searching book byId')
+    }
+     if(!book) {
+          res.send('no book exists');
+    }
+      if(comment){
+        book.comments.push(comment);
+        book.commentcount++;
+      }
+      book.save(error => {
+          if (error) {
+            return res.send('could not add comment');
+          }
+          return res.json({_id: book._id, title: book.title, comments: book.comments });
+        })
+    })
     })
     
     .delete(function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
-    });
+    if (!bookid) {
+        return res.send('please put id')
+      }
+     bookModel.deleteOne({ _id: bookid }, function(err){
+        if (err){
+          res.send('err when searching book byId')
+        }
+        return res.send('delete successful');
+   }) 
+  });
   
 };
