906893a7b9927de5fbc700048d704edbd609c360
Abdellah-web <54070353+Abdellah-web@users.noreply.github.com>
2019-09-16 14:37:14 +0200

    Update api.js

--- boilerplate-project-library-gomix/routes/api.js
+++ boilerplate-project-library-gomix/routes/api.js
@@ -14,13 +14,44 @@ var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
+var expect = require('chai').expect;
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect(process.env.DB, { useNewUrlParser: true });
+
+//create books schema and model
+const booksSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  commentcount: {
+    type: Number,
+    default: 0
+  },
+  comments: [],
+  updated_on :{ type: Number, required: true }
+});
+
+const booksModel = mongoose.model("Books", booksSchema);
+
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
+    booksModel.find({}, function (error, books) {
+        if (error) {
+          return res.send('no books found')
+        }
+        const result = books.map(book=>{return {_id:book._id, title: book.title, commentcount: book.commentcount }})
+       result.sort( { updated_on: 'desc' } ) 
+        return res.json(result)
+        })
+  })
     
     .post(function (req, res){
       var title = req.body.title;
