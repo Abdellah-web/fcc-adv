072f016ff1181fe9154fe0ab984ada75d7a7935b
Abdellah-web <54070353+Abdellah-web@users.noreply.github.com>
2019-09-17 19:00:36 +0200

    Update and rename Stok.js to Thread.js

---
+++ boilerplate-project-messageboard-gomix/models/Thread.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const threadSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  delete_password: { type: String, required: true },
+  replies: [{
+    text: { type: String, required: true },
+    delete_password: { type: String, required: true },
+    created_on: { type: Date, default: Date.now },
+    reported: { type: Boolean, default: false },
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: mongoose.Types.ObjectId
+    },
+  }],
+  created_on: { type: Date, default: Date.now },
+  bumped_on: { type: Date, default: Date.now },
+  reported: { type: Boolean, default: false },
+  board: { type: String, required: true },
+})
+
+const Thread = mongoose.model("Thread", threadSchema, "threads")
+
+module.exports = Thread
